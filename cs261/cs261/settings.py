a5e805ed56371ba3578e9d8d164b0e78e3cabbe3
Ash Brent-Carpenter <ash@sent.com>
2018-02-17 19:12:40 +0000

    add /cs261/templates to template dirs

--- cs261/cs261/settings.py
+++ cs261/cs261/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'cs261.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'cs261/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
