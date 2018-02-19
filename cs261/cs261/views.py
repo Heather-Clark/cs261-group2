c04e8abb2a0559d4caff1718996f1b4ccb98dd58
ashbc <ash@sent.com>
2018-02-19 13:36:18 +0000

    escape query in http response 2

--- cs261/cs261/views.py
+++ cs261/cs261/views.py
@@ -1,6 +1,16 @@
 from django.http import HttpResponse
+from django.utils.html import escape
 
 # Where the magic happens.
 # i.e. this is where we interface with the app logic.
 def query(request):
-    return HttpResponse('I don\'t know how to respond to "' + request.GET.get('q') + '".')
+    # if we're returning all or part of the query, we might need this to stop people XSSing themselves
+    escaped_query = escape(request.GET.get('q', ''))
+
+    # we don't escape the full output,
+    # so we can still use arbitrary HTML tags in our response
+    # which could be useful
+    response = """
+I don't know how to respond to {}.
+    """.format(escaped_query)
+    return HttpResponse(response)
