791d0948a0bce57c00d6bf744b5ac1e312e86511
ashbc <ash@sent.com>
2018-02-21 11:28:09 +0000

    switch response rendering to use a template

--- cs261/cs261/views.py
+++ cs261/cs261/views.py
@@ -1,16 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 from django.utils.html import escape
 
 # Where the magic happens.
 # i.e. this is where we interface with the app logic.
 def query(request):
-    # if we're returning all or part of the query, we might need this to stop people XSSing themselves
-    escaped_query = escape(request.GET.get('q', ''))
-
-    # we don't escape the full output,
-    # so we can still use arbitrary HTML tags in our response
-    # which could be useful
-    response = """
-I don't know how to respond to {}.
-    """.format(escaped_query)
-    return HttpResponse(response)
+    context = {
+        'query': request.GET.get('q', '?')
+    }
+    print(context)
+    return render(request, 'cs261/response.html', context=context)
