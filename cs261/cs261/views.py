5091912370f7ce949d9701d1d0183e1d85f0466b
ashbc <ash@sent.com>
2018-02-21 13:45:13 +0000

    implement dialogflow

--- cs261/cs261/views.py
+++ cs261/cs261/views.py
@@ -2,11 +2,36 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.utils.html import escape
 
+import requests
+import json
+import os
+
 # Where the magic happens.
 # i.e. this is where we interface with the app logic.
 def query(request):
-    context = {
-        'query': request.GET.get('q', '?')
+
+    params = {
+        'query': request.GET.get('q', 'No query'),
+        'sessionId': '1',
+        'v': '20150910',
+        'lang': 'en'
     }
-    print(context)
+
+    headers = {
+        'Authorization': 'Bearer ' + os.environ['BOT_TOKEN']
+    }
+
+    r = requests.get('https://api.dialogflow.com/v1/query', params=params, headers=headers)
+
+    text = json.loads(r.text)['result']['fulfillment']['speech']
+
+    context = {
+        'message': text
+    }
+
+    import datetime
+    with open('logs/' + datetime.datetime.now().strftime('%Y-%m-%d-T%H-%M-%S.json'), 'w+') as f:
+        f.write(r.text)
+
+
     return render(request, 'cs261/response.html', context=context)
