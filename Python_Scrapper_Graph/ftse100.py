4fad99f5f02a70027d200313082bc1ec54c2000e
jayzern <jayzern@hotmail.com>
2018-03-02 13:17:40 +0000

    Add files via upload
    update: clean code

--- Python_Scrapper_Graph/ftse100.py
+++ Python_Scrapper_Graph/ftse100.py
@@ -65,41 +65,10 @@ def get_data_from_yahoo(reload_ftse100 = False): # change this to True if u want
 		else:
 			print('Already have {}'.format(ticker))
 
-
-# EXAMPLE: BARCLAYS/BARC
-# Yahoo uses Barc.L as a ticker
-# extract
-start = dt.datetime(2000,1,1)
-end = dt.datetime.now()
-df = web.DataReader('BARC.L', 'yahoo', start, end)
-df.to_csv('barc.csv')
-
-# 100 day moving average
-df = pd.read_csv('barc.csv', parse_dates = True, index_col = 0)
-df['100ma'] = df['Adj Close'].rolling(window=100, min_periods=0).mean()
-
-# re sample open high low close candle stick graphs
-df_ohlc = df['Adj Close'].resample('10D').ohlc()
-df_volume = df['Volume'].resample('10D').sum()
-df_ohlc.reset_index(inplace=True)
-df_ohlc['Date'] = df_ohlc['Date'].map(mdates.date2num)
-
-# plot
-ax1 = plt.subplot2grid((6,1), (0,0), rowspan=5, colspan=1)
-ax2 = plt.subplot2grid((6,1), (5,0), rowspan=1, colspan=1, sharex = ax1)
-ax1.xaxis_date()
-
-# output
-candlestick_ohlc(ax1, df_ohlc.values, width=2, colorup='g')
-ax2.fill_between(df_volume.index.map(mdates.date2num), df_volume.values, 0)
-#plt.show()
-
-
 ################################################## Basic Statistics
-# TODO: The last row of the CSV is fucked up
 def get_current_spot_price(ticker):
 	df = pd.read_csv("ftse100tickers/"+ticker+".csv")
-	price = df.iloc[-2,4] # -2 = 2nd last row, 5 = column index
+	price = df.iloc[-2,4]
 	return price
 
 print("Current Spot Price of BARC is ", get_current_spot_price("BARC"))
@@ -125,7 +94,7 @@ def get_specific_spot_price(ticker, date):
 	price = row.iloc[0,4]
 	return price
 
-print("Specific spot price is", get_specific_spot_price("BARC", '2000-02-01'))
+print("Specific spot price in year 2000 is", get_specific_spot_price("BARC", '2000-02-01'))
 
 def get_specific_trading_volume(ticker, date):
 	df = pd.read_csv("ftse100tickers/"+ticker+".csv")
@@ -133,7 +102,7 @@ def get_specific_trading_volume(ticker, date):
 	volume = row.iloc[0,6]
 	return volume
 
-print("Specific trading volume is", get_specific_trading_volume("BARC", '2000-02-01'))
+print("Specific trading volume in year 2000 is", get_specific_trading_volume("BARC", '2000-02-01'))
 
 def get_profit_period(ticker):
 	response = urllib2.urlopen("https://query2.finance.yahoo.com/v10/finance/quoteSummary/"+ticker+".L?modules=financialData")
@@ -143,7 +112,7 @@ def get_profit_period(ticker):
 	profit = float(profitMargins) - float(revenue)
 	return profit
 
-print("profit this year is",get_profit_period("BARC"))
+print("Profit in 2017 is", get_profit_period("BARC"))
 
 def get_eps_year(ticker):
 	response = urllib2.urlopen("https://query2.finance.yahoo.com/v10/finance/quoteSummary/"+ticker+".L?modules=defaultKeyStatistics")
@@ -152,7 +121,7 @@ def get_eps_year(ticker):
 	forwardEps = json.dumps(data['quoteSummary']['result'][0]['defaultKeyStatistics']['forwardEps']['raw'])
 	return (trailingEps, forwardEps)
 
-print("trailing EPS and forward EPS is", get_eps_year("BARC"))
+print("Trailing EPS and forward EPS is", get_eps_year("BARC"))
 
 # OK this is probably wrong.... idk how to calculate div per share
 def get_dividend_per_share(ticker):
@@ -163,37 +132,31 @@ def get_dividend_per_share(ticker):
 	dps = -float(dividends) / float(outstandingShares)
 	return dps
 
-print("dividend per share for last year is", get_dividend_per_share("BARC"))
+print("Dividend per share in 2017 is", get_dividend_per_share("BARC"))
 
 ################################################## Group Statistics
-
-#TODO: web scrapping HashMap - <ticker, industry>
-#def get_industry_trend(industry):
+def get_industry_trend(industry):
 
 #def get_industry_trend_performance(industry, period):
 
 #def get_companies_industry_trend(industry, trend=True): # True - Rising, False - Falling
 
 # Helper Function
-def get_companies(industry):
+def get_tickers(industry):
 	resp = requests.get('https://en.wikipedia.org/wiki/FTSE_100_Index')
 	soup = bs.BeautifulSoup(resp.text, "lxml")
 	table = soup.find('table', {'class': 'wikitable sortable'})
 	tickers = []
 	for row in table.findAll('tr')[1:]:
-		sector = row.findAll('td')[2]
-		#print(sector)
-
-
-		ticker = row.findAll('td')[1].text
-		if ticker.endswith('.'):
-			ticker = ticker[:-1]
-		ticker = ticker.replace('.','-')
-		tickers.append(ticker)
+		if(row('td')[2].text == industry):
+			ticker = row('td')[1].text
+			if ticker.endswith('.'):
+				ticker = ticker[:-1]
+			ticker = ticker.replace('.','-')
+			tickers.append(ticker)
 
 	with open("sp500tickers.pickle", "wb") as f:
 		pickle.dump(tickers, f)
+	return tickers
 
-	#print(tickers)
-
-#get_companies("Banks")
+print("Companies in 'Banks' industry are:", get_tickers("Banks"))
